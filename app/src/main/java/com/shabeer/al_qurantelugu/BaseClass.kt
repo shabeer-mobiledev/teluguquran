ae27ba85b20ff918c1fe427cc293178b50a3689e
Shaik Shabeer <shabeer.mobiledev25@gmail.com>
2024-12-07 14:51:03 +0530

    initial commit

---
+++ app/src/main/java/com/shabeer/al_qurantelugu/BaseClass.kt
@@ -0,0 +1,8 @@
+package com.shabeer.al_qurantelugu
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseClass : Application() {
+}
---
+++ app/src/main/java/com/shabeer/al_qurantelugu/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.shabeer.al_qurantelugu
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.shabeer.al_qurantelugu.ui.theme.TeluguquranTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TeluguquranTheme {
+                Surface(
+                    color = MaterialTheme.colorScheme.surface,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+
+                }
+            }
+        }
+    }
+}
